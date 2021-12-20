1cd4172ce600caa4ccd76292d0abdb7a5403b0f6
Ed Carney <carneyedwardj@gmail,com>
2021-12-20 16:01:54 -0500

    Adds some defaults and a test HTML page

--- HealthCheck/HealthCheck/Program.cs
+++ HealthCheck/HealthCheck/Program.cs
@@ -1,25 +1,46 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.AspNetCore.SpaServices.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
+builder.Services.AddSpaStaticFiles(opt =>
+{
+    opt.RootPath = "src/assets/";
+});
 
 var app = builder.Build();
+var env = app.Environment;
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!env.IsDevelopment())
 {
 }
 
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+if (env.IsDevelopment())
+{
+    app.UseSpaStaticFiles();
+}
+
 app.UseRouting();
-
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html"); ;
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+    if (env.IsDevelopment())
+    {
+        spa.UseAngularCliServer("start");
+    }
+});
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
 
