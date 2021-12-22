556235cf13e8dd18d8c19880228ec77dae21c9de
Ed Carney <carneyedwardj@gmail,com>
2021-12-22 12:02:03 -0500

    Restores to functional state

--- HealthCheck/HealthCheck/Program.cs
+++ HealthCheck/HealthCheck/Program.cs
@@ -1,46 +1,25 @@
-﻿using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.AspNetCore.SpaServices.Extensions;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddSpaStaticFiles(opt =>
-{
-    opt.RootPath = "src/assets/";
-});
 
 var app = builder.Build();
-var env = app.Environment;
 
 // Configure the HTTP request pipeline.
-if (!env.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
 }
 
-//app.UseHttpsRedirection();
 app.UseStaticFiles();
-
-if (env.IsDevelopment())
-{
-    app.UseSpaStaticFiles();
-}
-
 app.UseRouting();
+
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.UseSpa(spa =>
-{
-    spa.Options.SourcePath = "ClientApp";
-    if (env.IsDevelopment())
-    {
-        spa.UseAngularCliServer("start");
-    }
-});
-
-app.MapFallbackToFile("index.html");
+app.MapFallbackToFile("index.html"); ;
 
 app.Run();
 
