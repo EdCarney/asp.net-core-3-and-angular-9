b048ecdc60213d0f55d8053e12ea96d0d30d4bd2
Ed Carney <carneyedwardj@gmail,com>
2021-12-28 06:55:42 -0800

    Updates health check to provide more granular JSON information

--- HealthCheck/HealthCheck/ICMPHealthCheck.cs
+++ HealthCheck/HealthCheck/ICMPHealthCheck.cs
@@ -2,12 +2,20 @@
 using System.Net.NetworkInformation;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-namespace HealthCheck.Models
+namespace HealthCheck
 {
 	public class ICMPHealthCheck : IHealthCheck
 	{
-        private string Host = "www.asdihg.com";
-        private int Timeout = 300;
+        string Host { get; }
+        int TimeoutMs { get; }
+
+        private string ErrorMsg => $"ICMP to {Host} failed";
+
+        public ICMPHealthCheck(string host, int timeoutMs)
+        {
+            Host = host ?? string.Empty;
+            TimeoutMs = timeoutMs;
+        }
 
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
@@ -22,17 +30,18 @@ namespace HealthCheck.Models
                 switch (reply?.Status)
                 {
                     case IPStatus.Success:
-                        return reply.RoundtripTime < Timeout
-                            ? HealthCheckResult.Healthy()
-                            : HealthCheckResult.Degraded();
+                        string msg = $"ICMP to {Host} took {reply.RoundtripTime}";
+                        return reply.RoundtripTime < TimeoutMs
+                            ? HealthCheckResult.Healthy(msg)
+                            : HealthCheckResult.Degraded(msg);
                     default:
-                        return HealthCheckResult.Unhealthy();
+                        return HealthCheckResult.Unhealthy(ErrorMsg);
                 }
             }
             catch (Exception ex)
             {
                 // log exception
-                return HealthCheckResult.Unhealthy();
+                return HealthCheckResult.Unhealthy(ErrorMsg, ex);
             }
         }
     }
