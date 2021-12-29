efd3642f02037e15e216042efc7ad56e78b814d5
Ed Carney <edcarney@microsoft.com>
2021-12-29 16:52:27 -0500

    Moves the model files for the configuration and adds new model files for the City and Country models

---
+++ HealthCheck/HealthCheck/Models/Data/City.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCheck.Models.Data
+{
+    public class City
+    {
+        /// <summary>
+        /// Unique ID and primary key for the city
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// City name (in UTF8 format)
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// City name (in ASCII format)
+        /// </summary>
+        public string Name_ASCII { get; set; }
+
+        /// <summary>
+        /// City latitude
+        /// </summary>
+        public decimal Lat { get; set; }
+
+        /// <summary>
+        /// City longitude
+        /// </summary>
+        public decimal Lon { get; set; }
+
+        /// <summary>
+        /// Country ID (foreign key)
+        /// </summary>
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+    }
+}
---
+++ HealthCheck/HealthCheck/Models/Data/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCheck.Models.Data
+{
+    public class Country
+    {
+        /// <summary>
+        /// Unique ID and primary key for the country
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Country name (in UTF8 format)
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-2 format)
+        /// </summary>
+        public string ISO2 { get; set; }
+
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-3 format)
+        /// </summary>
+        public string ISO3 { get; set; }
+    }
+}
