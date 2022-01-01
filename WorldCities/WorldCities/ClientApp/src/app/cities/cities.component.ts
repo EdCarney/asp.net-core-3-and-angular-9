fa3433b4f64bbc858b54df52bdfe32ec6e457a1a
Ed Carney <carneyedwardj@gmail,com>
2022-01-01 16:17:08 -0500

    Adds client-side sorting

--- WorldCities/WorldCities/ClientApp/src/app/cities/cities.component.ts
+++ WorldCities/WorldCities/ClientApp/src/app/cities/cities.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, ViewChild } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, SortDirection } from '@angular/material/sort';
 
 import { City } from './city';
 import { ApiResult } from '../apiResult';
@@ -12,11 +13,17 @@ import { ApiResult } from '../apiResult';
   styleUrls: ['./cities.component.css']
 })
 export class CitiesComponent {
-  @ViewChild('paginator') paginator: MatPaginator;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
   public cities: City[];
   public dataSource: MatTableDataSource<City>;
+
+  private readonly defaultPageIndex: number = 0;
+  private readonly defaultPageSize: number = 10;
+  public readonly defaultSortColumn: string = "";
+  public readonly defaultSortOrder: SortDirection = "" as SortDirection;
 
   constructor(
     private http: HttpClient,
@@ -24,21 +31,23 @@ export class CitiesComponent {
   }
 
   ngOnInit() {
+    this.loadData();
+  }
+
+  public loadData(): void {
     let pageEvent: PageEvent = new PageEvent();
     pageEvent.pageIndex = 0;
     pageEvent.pageSize = 10;
     this.getData(pageEvent);
   }
 
-  public citiesLoaded(): boolean {
-    return this.cities && this.cities.length > 0;
-  }
-
-  public getData(event: PageEvent) {
+  public getData(event: PageEvent): void {
     let url: string = this.baseUrl + "api/cities";
     let params: HttpParams = new HttpParams()
       .set("pageIndex", event.pageIndex.toString())
-      .set("pageSize", event.pageSize.toString());
+      .set("pageSize", event.pageSize.toString())
+      .set("sortColumn", this.sort ? this.sort.active : this.defaultSortColumn)
+      .set("sortOrder", this.sort ? this.sort.direction : this.defaultSortOrder);
 
     this.http.get<ApiResult<City>>(url, { params })
       .subscribe(
@@ -51,5 +60,9 @@ export class CitiesComponent {
         },
         error => console.error(error)
     );
+  }
+
+  public citiesLoaded(): boolean {
+    return this.cities && this.cities.length > 0;
   }
 }
