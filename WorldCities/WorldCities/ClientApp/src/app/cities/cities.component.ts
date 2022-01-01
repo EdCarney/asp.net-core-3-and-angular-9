68f4db9921e5f4cb5b962755f8f4a35478598ddd
Ed Carney <carneyedwardj@gmail,com>
2022-01-01 10:53:13 -0500

    Adds client-side pagination

--- WorldCities/WorldCities/ClientApp/src/app/cities/cities.component.ts
+++ WorldCities/WorldCities/ClientApp/src/app/cities/cities.component.ts
@@ -1,6 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { City } from './city';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-cities',
@@ -8,20 +10,30 @@ import { City } from './city';
   styleUrls: ['./cities.component.css']
 })
 export class CitiesComponent {
+  @ViewChild('paginator') paginator: MatPaginator;
+
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
   public cities: City[];
+  public dataSource: MatTableDataSource<City>;
+
+  public citiesLoaded(): boolean {
+    return this.cities && this.cities.length > 0;
+  }
 
   constructor(
     private http: HttpClient,
     @Inject('BASE_URL') private baseUrl: string) {
-    this.cities = [];
   }
 
   ngOnInit() {
     this.http.get<City[]>(this.baseUrl + 'api/cities')
       .subscribe(
-        result => this.cities = result,
+        result => {
+          this.cities = result;
+          this.dataSource = new MatTableDataSource(this.cities);
+          this.dataSource.paginator = this.paginator;
+        },
         error => console.error(error)
-      );
+    );
   }
 }
